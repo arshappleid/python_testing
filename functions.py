d1672ec81d21f04f565fbca2aa984518c0eaf4ba
Prabhmeet  Deol <deol@Prabhmeets-MacBook-Air.local>
2022-03-12 16:50:40 -0800

    fixed bugs in test

--- functions.py
+++ functions.py
@@ -1,9 +1,10 @@
-
 ## all the functions that we create, or would want to use for our project. Would be added here.
+import math
 
-def add(num1, num2):
+
+def add(num1, num2)-> int: ## will return int type
 	return (num1+num2);
 
-def add_Strings(str1,str2):
+def add_Strings(str1,str2)-> str: ## will return string
 	return (str1+" "+str2);
 
